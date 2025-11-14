a61d2bd940be4e513f394597d6901d33ddd2806e
PixelThrived <pixelthrived@gmail.com>
2025-11-14 06:18:18 +0200

    Revert "hardware: dolby: Fix build for Android 15"
    This reverts commit 04d2bde4b5ea969e0829e4dc73b1adeaaa6f4de2.

--- dolby/DolbyManager/src/co/aospa/dolby/geq/ui/EqualizerScreen.kt
+++ dolby/DolbyManager/src/co/aospa/dolby/geq/ui/EqualizerScreen.kt
@@ -11,12 +11,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.android.settingslib.spa.framework.theme.SettingsDimension
 import com.android.settingslib.spa.framework.theme.SettingsTheme
 
 @Composable
@@ -24,21 +22,19 @@ fun EqualizerScreen(
     viewModel: EqualizerViewModel,
     modifier: Modifier = Modifier
 ) {
-    SettingsTheme {
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(SettingsDimension.itemPadding)
-                .then(modifier),
-            color = MaterialTheme.colorScheme.background
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 8.dp)
+            .then(modifier),
+        color = SettingsTheme.colorScheme.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxHeight()
         ) {
-            Column(
-                verticalArrangement = Arrangement.Top,
-                modifier = Modifier.fillMaxHeight()
-            ) {
-                PresetSelector(viewModel = viewModel)
-                EqualizerBands(viewModel = viewModel)
-            }
+            PresetSelector(viewModel = viewModel)
+            EqualizerBands(viewModel = viewModel)
         }
     }
 }
